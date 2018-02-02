4b609b135de8050e6d2c2d29388108ff1298db8f
gojutin <gojutin@gmail.com>
2018-02-02 10:53:34 -0500

    ES5 conversion for bundlers

--- index.js
+++ index.js
@@ -1,57 +1,85 @@
-const crudReducer = name => (state = {}, action) => {
-  const reducerName = name.toUpperCase();
-  const methodName = action.payload && action.payload.methodName;
-  switch (action.type) {
-    case `${reducerName}_ACTION_PENDING`:
-      return Object.assign({}, state, {
-        [`${methodName}`]: {
-          pending: true,
-          data: null,
-          error: null,
-        },
-      });
-    case `${reducerName}_ACTION_COMPLETE`:
-      return Object.assign({}, state, {
-        [`${methodName}`]: {
-          pending: false,
-          data: action.payload.data,
-          error: null,
-        },
-      });
-    case `${reducerName}_ACTION_ERROR`:
-      return Object.assign({}, state, {
-        [`${methodName}`]: {
-          pending: false,
-          data: state.data,
-          error: action.payload.error,
-        },
-      });
-    default:
-      return state;
+'use strict';
+
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true,
+    });
+  } else {
+    obj[key] = value;
   }
+  return obj;
+}
+
+var crudReducer = function crudReducer(name) {
+  return function() {
+    var state =
+      arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+    var action = arguments[1];
+
+    var reducerName = name.toUpperCase();
+    var methodName = action.payload && action.payload.methodName;
+    switch (action.type) {
+      case reducerName + '_ACTION_PENDING':
+        return Object.assign(
+          {},
+          state,
+          _defineProperty({}, '' + methodName, {
+            pending: true,
+            data: null,
+            error: null,
+          }),
+        );
+      case reducerName + '_ACTION_COMPLETE':
+        return Object.assign(
+          {},
+          state,
+          _defineProperty({}, '' + methodName, {
+            pending: false,
+            data: action.payload.data,
+            error: null,
+          }),
+        );
+      case reducerName + '_ACTION_ERROR':
+        return Object.assign(
+          {},
+          state,
+          _defineProperty({}, '' + methodName, {
+            pending: false,
+            data: state.data,
+            error: action.payload.error,
+          }),
+        );
+      default:
+        return state;
+    }
+  };
 };
 
-const crudAction = (name, promise) => {
-  return async dispatch => {
-    const reducerName = name.split('.')[0].toUpperCase();
-    const methodName = name.split('.')[1];
+var crudAction = function crudAction(name, promise) {
+  return async function(dispatch) {
+    var reducerName = name.split('.')[0].toUpperCase();
+    var methodName = name.split('.')[1];
     try {
       dispatch({
-        type: `${reducerName}_ACTION_PENDING`,
-        payload: { methodName },
+        type: reducerName + '_ACTION_PENDING',
+        payload: { methodName: methodName },
       });
-      const results = await promise();
+      var results = await promise();
       dispatch({
-        type: `${reducerName}_ACTION_COMPLETE`,
-        payload: { methodName, data: results },
+        type: reducerName + '_ACTION_COMPLETE',
+        payload: { methodName: methodName, data: results },
       });
     } catch (error) {
       dispatch({
-        type: `${reducerName}_ACTION_ERROR`,
-        payload: { methodName, error },
+        type: reducerName + '_ACTION_ERROR',
+        payload: { methodName: methodName, error: error },
       });
     }
   };
 };
 
-module.exports = { crudReducer, crudAction };
+module.exports = { crudReducer: crudReducer, crudAction: crudAction };
